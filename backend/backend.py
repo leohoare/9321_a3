1c8821c294de8166dcf5d699a7b579882ccf88dc
Leo Hoare <leo.hoare@live.com>
2019-04-01 11:08:45 +1100

    working on backend + requirements

--- backend/backend.py
+++ backend/backend.py
@@ -1,8 +1,15 @@
 import os
 import csv
+import re
+import io
+import pandas as pd
+### ADD SEABORN PLOT / MATPLOTLIB TO REQUIREMENTS IF WE WANT IT ###
+import seaborn as sns
+import matplotlib.pyplot as plt
 from flask import Flask, abort, request
 from flask_restplus import Resource, Api, reqparse, fields
 from flask_sqlalchemy import SQLAlchemy
+
 
 
 def create_db(db_file):
@@ -16,19 +23,19 @@ def create_db(db_file):
     # nullable for now
     class Entry(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        age = db.Column(db.Integer, nullable=True)
-        sex = db.Column(db.Boolean, nullable=True)
-        cp = db.Column(db.Integer, nullable=True)       
-        restbp = db.Column(db.Integer, nullable=True)
-        chol = db.Column(db.Integer, nullable=True)
-        fbs = db.Column(db.Boolean, nullable=True)
-        restecg = db.Column(db.Integer, nullable=True)
-        maxhr = db.Column(db.Integer, nullable=True)
-        exang = db.Column(db.Boolean, nullable=True)
-        oldpeak = db.Column(db.Float(4,4), nullable=True)
-        slope = db.Column(db.Integer, nullable=True)
-        numves = db.Column(db.Integer, nullable=True)
-        thal = db.Column (db.Integer, nullable=True)
+        age = db.Column(db.Integer, nullable=False)
+        sex = db.Column(db.Boolean, nullable=False)
+        cp = db.Column(db.Integer, nullable=False)       
+        restbp = db.Column(db.Integer, nullable=False)
+        chol = db.Column(db.Integer, nullable=False)
+        fbs = db.Column(db.Boolean, nullable=False)
+        restecg = db.Column(db.Integer, nullable=False)
+        maxhr = db.Column(db.Integer, nullable=False)
+        exang = db.Column(db.Boolean, nullable=False)
+        oldpeak = db.Column(db.Float(4,4), nullable=False)
+        slope = db.Column(db.Integer, nullable=False)
+        numves = db.Column(db.Integer, nullable=False)
+        thal = db.Column (db.Integer, nullable=False)
         # target = db.Column (db.Integer, nullable=True)
 
     db.create_all()
@@ -39,7 +46,8 @@ def populate_db(file_path):
     with open(file_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            db.session.add(Entry(\
+            try:
+                db.session.add(Entry(\
                 age = row[0], \
                 sex = bool(row[1]), \
                 cp = row[2], \
@@ -53,7 +61,9 @@ def populate_db(file_path):
                 slope = row[10], \
                 numves = row[11], \
                 thal = row[12], \
-                ))
+                    ))
+            except Exception as ex:
+                print(ex) 
     db.session.commit()
 
 "API "
@@ -62,14 +72,56 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app, title='Backend for 9321 a3', description='', default="Actions",  default_label=None)
     
 
-@api.route('/test')
-class VisualiseGraph(Resource):
-    # @api.doc(body=api.model("payload", {"indicator_id":fields.String(description="indicator_Id",required=True)}),\
-    #     responses={200: 'Success', 400: 'Incorrect input by user'})
-    def post(self):
-        return 200
+@api.route('/getdata/<string:agesex>/<int:indicator>')
+class DataAcesss(Resource):
+    @api.doc(responses={200: 'Success', 400: 'Incorrect input by user'})
+    def get(self,agesex,indicator):
+        if agesex.lower() == 'sex' or agesex == '1':
+            agesex = 1
+        elif agesex.lower() == 'age' or agesex == '2':
+            agesex = 2
+        else:
+            abort(400, 'agesex must be in set {age,sex,1,2}')
+        if indicator > 13 or indicator < 3:
+            abort(400, 'indicator must be between 3 and 13')
+        col1 = df.columns[agesex-1]
+        col2 = df.columns[indicator-1]
+        return {
+
+            "records":
+            [   
+                {
+                    col1 : row[col1],
+                    col2 : row[col2],
+                }
+                for index, row in df[[df.columns[agesex-1], df.columns[indicator-1]]].iterrows()
+            ]
+        }, 200
+    
+@api.route('/getgraph/<string:agesex>/<int:indicator>')
+class DataAcesss(Resource):
+    @api.doc(responses={200: 'Success', 400: 'Incorrect input by user'})
+
+    # NOT WORKING YET
+    def get(self,agesex,indicator):
+        if agesex.lower() == 'sex' or agesex == '1':
+            agesex = 1
+        elif agesex.lower() == 'age' or agesex == '2':
+            agesex = 2
+        else:
+            abort(400, 'agesex must be in set {age,sex,1,2}')
+        if indicator > 13 or indicator < 3:
+            abort(400, 'indicator must be between 3 and 13')
+        col1 = df.columns[agesex-1]
+        col2 = df.columns[indicator-1]
+        print(df["age"],df["chol"])
+        bytes_image = io.BytesIO()
+        sns.regplot(x=df["age"], y=df["chol"])
+        plt.savefig(bytes_image, format='png')
+        return bytes_image.seek(0), 200
 
 if __name__ == '__main__':
-    create_db('data.db')
-    populate_db('../data/processed.cleveland.data')
+    # create_db('data.db')
+    # populate_db('../data/processed.cleveland.data')
+    df = pd.read_csv("./../data/analytics.csv")
     app.run(debug=True)
