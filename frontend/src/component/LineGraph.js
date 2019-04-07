081b9a787aab32ba1fe5b13980990a1739f13a35
wing4ever <z5091907@unsw.edu.au>
2019-04-07 13:59:16 +1000

    update be

--- frontend/src/component/LineGraph.js
+++ frontend/src/component/LineGraph.js
@@ -1,52 +1,47 @@
 import React, { Component } from 'react';
-import {Chart,Geom,Axis,Tooltip, Legend} from "bizcharts";
+import {Chart,Geom,Axis,Tooltip} from "bizcharts";
+// import {Layout, Select} from "antd";
+
+// const {Header, Content} = Layout;
+// const Option = Select.Option;
 
 // fake data
 const data = [
     {
       age: "10",
       value: 30,
-      sex:"F"
     },
     {
       age: "20",
       value: 40,
-      sex:"F"
     },
     {
       age: "30",
       value: 35,
-      sex:"F"
     },
     {
       age: "40",
       value: 50,
-      sex:"F"
     },
     {
       age: "50",
       value: 49,
-      sex:"M"
     },
     {
       age: "60",
       value: 60,
-      sex:"M"
     },
     {
       age: "70",
       value: 70,
-      sex:"M"
     },
     {
       age: "80",
       value: 90,
-      sex:"F"
     },
     {
       age: "90",
       value: 130,
-      sex:"M"
     }
   ];
   const cols = {
@@ -58,34 +53,49 @@ const data = [
     }
   };
 
+// {/* <Header>
+//     <Select defaultValue="both" style={{ width: 120 }} onChange={this.handleChange}>
+//         <Option value="male">male</Option>
+//         <Option value="female">female</Option>
+//         <Option value="both" >any sex</Option>                    <Option value="Yiminghe">yiminghe</Option>
+//     </Select>
+// </Header> */}
+
 class LineGraph extends Component {
-  render() {
-    return (
+    constructor(props) {
+        super(props);
+        this.state = {
+            sex:"both"
+        };
+    }
+
+
+    render() {
+      return (
         <div>
-        <Chart height={400} data={data} scale={cols} forceFit>
+          <Chart height={400} data={data} scale={cols} forceFit>
           <Axis name="age" title />
           <Axis name="value" title />
           <Tooltip
-            crosshairs={{
+              crosshairs={{
               type: "y"
-            }}
+              }}
           />
           <Geom type="line" position="age*value" size={2} />
           <Geom
-            type="point"
-            position="age*value"
-            size={4}
-            shape={"circle"}
-            style={{
+              type="point"
+              position="age*value"
+              size={4}
+              shape={"circle"}
+              style={{
               stroke: "#fff",
               lineWidth: 1
-            }}
+              }}
           />
-          <Legend/>
-        </Chart>
-      </div>
-    );
-  }
+          </Chart>
+        </div>
+      );
+    }
 }
 
 export default LineGraph;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
-import "./index.css";
+// import "./index.css";
 import "antd/dist/antd.css";
 
 ReactDOM.render(
@@ -18,4 +18,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
