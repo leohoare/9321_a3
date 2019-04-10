0ab8a4c4d71d14b1fef23a1f41454e6995dc33d4
Amit Lamichhane <Lamichhaneamit7@gmail.com>
2019-04-10 12:46:51 +1000

    Feature_extraction with diagram
    For data in feature extraction

--- model/test.py
+++ model/test.py
@@ -1,8 +1,12 @@
-import train
+from model import prediction_clean_data
+from train import knn
+from train import train_random_forest
 import pandas as pd
 
 
-
+"""
+testing randdom forest 
+"""
 
 
 
@@ -12,10 +16,16 @@ def run_test():
     analytics = analytics.head(1)
     y_predict = analytics['target']
     analytics = analytics.drop('target', axis=1)
-    aa = train.predict_Data(analytics)
+    df_norm = pd.read_csv("./../data/normalised.csv")
+    df_model = pd.read_csv("./../data/model.csv")
+    analytics = prediction_clean_data(analytics, df_norm)
+    knn_a = knn(df_model,analytics)
 
-    print (y_predict[0])
-    print (aa[0])
+    random = train_random_forest(df_model,analytics)
+
+    print(random)
+    print(y_predict)
+
 
 
 if __name__ == "__main__":
